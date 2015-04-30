e686faebc90ff9904a0dc43c1a9863dc53f0c179
Tim Morgan <tim@timmorgan.org>
2015-04-29 21:44:28 -0500

    Remove rinari

--- tim/packages.el
+++ tim/packages.el
@@ -12,7 +12,6 @@
 
 (defvar tim-packages
   '(
-    rinari
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
